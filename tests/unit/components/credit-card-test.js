97a56b2b7b53667b54cb3c136e1cb6467a31733a
Esteban Arango Medina <marranoparael31@gmail.com>
2015-06-07 22:49:14 -0500

    Adding tests

--- tests/unit/components/credit-card-test.js
+++ tests/unit/components/credit-card-test.js
@@ -22,5 +22,40 @@ describeComponent(
       this.render();
       expect(component._state).to.equal('inDOM');
     });
+
+    it('accetps `width` option', function() {
+      // creates the component instance
+      var component = this.subject(),
+          card = null;
+      component.set('width',111);
+      this.render();
+      card = component.get('card');
+      expect(card.options.width).to.equal(111);
+
+    });
+
+    it('accetps `formatting` option', function() {
+      // creates the component instance
+      var component = this.subject(),
+          card = null;
+      component.set('formatting',false);
+      this.render();
+      card = component.get('card');
+      expect(card.options.formatting).to.equal(false);
+
+    });
+
+    it('accetps `values` option', function() {
+      // creates the component instance
+      var component = this.subject(),
+          card = null;
+      component.set('values',{number:'nuuuumber'});
+      this.render();
+      card = component.get('card');
+      console.log(card);
+      expect(card.options.values.number).to.equal('nuuuumber');
+
+    });
+
   }
 );
